f1918350bc6afd121aad3b8ff2afcd8c6509ddca
Yuta <y.nakagawa1107@gmail.com>
2018-11-18 09:21:23 +0900

    def のやり方2

--- hello-function.py
+++ hello-function.py
@@ -11,3 +11,23 @@ def say_hello(name):
 
 say_hello("Bob")
 say_hello("yuta")
+
+
+def double(number):
+    return 2 * number
+
+
+double(3)
+
+result_1 = double(3)
+
+print(result_1)
+
+
+def str_conbine(str1, str2):
+    return str1, str2
+
+
+result = str_conbine("Kazuma", "Takahasi")
+
+print(result)
